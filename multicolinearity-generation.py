9409c93b837abfbaa0c8790f21321eaffed7bd9e
Joshua Austin <josh.karl.austin@gmail.com>
2020-12-11 15:34:36 -0600

    Added heading space and package imports

--- multicolinearity-generation.py
+++ multicolinearity-generation.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 14:15:46 2020
+
+@author: Josh
+"""
+
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import StandardScaler
 
