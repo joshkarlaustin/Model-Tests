e9910bd700f54df9b284edb0114dcf86c42d8e11
Joshua Austin <josh.karl.austin@gmail.com>
2020-12-11 15:39:42 -0600

    Added declarations of constants

--- multicolinearity-generation.py
+++ multicolinearity-generation.py
@@ -13,3 +13,34 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import StandardScaler
 
+
+betE = 1
+bet2 = 1
+bet3 = 1
+bet4 = 1
+bet5 = -1
+bet6 = 1
+bet7 = 1
+
+muE = 0
+mu2 = 0
+mu3 = 0
+mu4 = 0
+mu5 = 0
+mu6 = 0
+mu7 = 0
+
+sdE = 3
+sd2 = 1
+sd3 = 1
+sd4 = 1
+sd5 = 1
+sd6 = 1
+sd7 = 1
+
+bias2 = 0
+bias3 = 0
+bias4 = 0
+bias5 = 0
+bias6 = 0
+bias7 = 0
